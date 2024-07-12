3a2f8201f23822c69ca88b6c0402b2f4e3e1fe70
Derek Chen <derex1987@gmail.com>
2024-07-12 17:27:58 -0500

    feat: lots of work on app

--- my-app/app/_layout.tsx
+++ my-app/app/_layout.tsx
@@ -2,8 +2,19 @@ import { Stack } from "expo-router";
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="index" />
+    <Stack
+      screenOptions={{
+        // Hide the header for all other routes.
+        headerShown: false,
+      }}
+    >
+      <Stack.Screen
+        name="index"
+        options={{
+          // Hide the header for this route
+          headerShown: false,
+        }}
+      />
     </Stack>
   );
 }
--- my-app/app/index.tsx
+++ my-app/app/index.tsx
@@ -1,15 +1,58 @@
-import { Text, View } from "react-native";
+import React from "react";
+import { useWindowDimensions, View, StyleSheet, Image, Text } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+import { FaMicrophone } from "react-icons/fa";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <LinearGradient
+        colors={["#1e3a8a", "#d946ef"]}
+        style={styles.gradient}
+        start={{ x: 0, y: 0 }}
+        end={{ x: 1, y: 0 }}
+      >
+        <Image
+          source={require("./../assets/images/logo.png")}
+          style={[styles.image]}
+          resizeMode="contain"
+        />
+      </LinearGradient>
+      <View style={styles.chatArea}>
+        <Text style={[styles.h1, {textAlign: 'left'}]}>Live Transcription:</Text>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+  },
+  chatArea: {
+    maxWidth: 375, // Fixed width representing a typical mobile phone screen
+  },
+  gradient: {
+    height: 128, 
+    width: "100%",
+    alignItems: "center",
+    justifyContent: "center",
+    marginBottom: 10,
+  },
+  image: {
+    // height: "100%",
+    // width: "100%",
+  },
+  h1: {
+    fontStyle: 'normal',
+    fontWeight: '700',
+    fontSize: 24,
+    lineHeight: 22, 
+    color: '#000000',
+    textShadowColor: 'rgba(0, 0, 0, 0.25)',
+    textShadowOffset: { width: 0, height: 10 },
+    textShadowRadius: 10,
+    zIndex: 11,
+  }
+});
